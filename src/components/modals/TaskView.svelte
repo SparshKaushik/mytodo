b7c6471eb96cd18e7c1b1ba86a64ec663661966e
Sparsh Kaushik <contact@sparshkaushik.com>
2023-05-06 23:38:37 +0530

    Add Editing Indicator, AutoSave in TaskView Modal

--- src/components/modals/TaskView.svelte
+++ src/components/modals/TaskView.svelte
@@ -6,10 +6,12 @@
 	import { supabase } from '../../supabase';
 	import { authStore, modalStore } from '$lib/stores';
 	import { taskHandlers } from '$lib/model';
+	import { draw } from 'svelte/transition';
 
 	let task: task_t;
 	let isEditing: boolean = false;
 	let descriptionDiv: HTMLDivElement;
+	let editingTimeout: any;
 
 	onMount(() => {
 		supabase
@@ -43,6 +45,25 @@
 			<span class="created">Created At : {new Date(task.createdAt).toLocaleString()}</span>
 		</span>
 		<div class="end">
+			{#if isEditing}
+				<div class="editing">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						class="edit-pencil"
+					>
+						<path d="M16 3 L21 8 L8 21 L3 21 L3 16 L16 3 Z" />
+					</svg>
+					<span> Editing </span>
+				</div>
+			{/if}
 			<IconButton
 				on:click={() => {
 					$authStore?.user.id && taskHandlers.deleteTask(task?.id, $authStore?.user.id);
@@ -82,7 +103,7 @@
 	</div>
 	<div class="body">
 		<div
-			class="description"
+			class="description {isEditing ? 'editing' : ''}"
 			on:dblclick={() => {
 				isEditing = true;
 			}}
@@ -97,6 +118,17 @@
 				isEditing = false;
 			}}
 			bind:this={descriptionDiv}
+			on:input={() => {
+				clearTimeout(editingTimeout);
+				editingTimeout = setTimeout(() => {
+					$authStore?.user.id &&
+						taskHandlers.updateTaskDescription(
+							task?.id,
+							descriptionDiv.innerText,
+							$authStore?.user.id
+						);
+				}, 3000);
+			}}
 		>
 			{task?.description}
 		</div>
@@ -130,6 +162,46 @@
 
 			gap: 0.5rem;
 
+			.editing {
+				display: flex;
+				align-items: center;
+				gap: 0.25rem;
+
+				padding: 0.25rem 0.5rem;
+				border-radius: 0.25rem;
+				background-color: rgba($color: #fff, $alpha: 0.2);
+
+				.edit-pencil {
+					path {
+						stroke-dasharray: 54;
+						animation: draw 6s ease-in-out forwards infinite;
+					}
+					@keyframes draw {
+						0% {
+							stroke-dashoffset: 0;
+						}
+						25% {
+							stroke-dashoffset: 54;
+						}
+						50% {
+							stroke-dashoffset: 108;
+						}
+						75% {
+							stroke-dashoffset: 54;
+						}
+						100% {
+							stroke-dashoffset: 0;
+						}
+					}
+				}
+
+				span {
+					font-size: 0.75rem;
+					font-weight: 600;
+					line-height: 1rem;
+				}
+			}
+
 			svg {
 				width: 1.25rem;
 				height: 1.25rem;
@@ -141,16 +213,19 @@
 		height: 100%;
 		display: flex;
 		flex-direction: column;
-
-		padding: 0.5rem;
-
 		overflow: auto;
 
 		.description {
 			height: 100%;
 			font-size: 1rem;
 			font-weight: 400;
+			padding: 0.25rem;
 			white-space: pre-wrap;
+
+			&.editing {
+				border: 1px solid #a0a0a0;
+				border-radius: 0.25rem;
+			}
 
 			&:focus {
 				outline: none;
